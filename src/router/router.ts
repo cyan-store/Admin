9039eef3283b366e0cc90a75b937d810dc72d539
Cyan903 <cyanp0515@gmail.com>
2023-09-07 03:26:38 -0400

    feat: Add cors

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,9 +1,17 @@
 import Fastify from "fastify";
 import jwt from "@fastify/jwt";
+import cors from "@fastify/cors";
 
 const fastify = Fastify();
 const secret = process.env.JWT_SECRET;
+const origin = process.env.CORS_ALLOW.split(",");
 
 fastify.register(jwt, { secret });
+fastify.register(cors, {
+    origin,
+    methods: ["GET", "POST", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Accept", "Authorization", "Content-Type"],
+    maxAge: 100,
+});
 
 export default fastify;
